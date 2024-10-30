0e063a3bffd8f136db18c6560c0a080904769777
Vincent Lin <vincent@pockyt.io>
2024-10-29 20:10:16 -0400

    fonts

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="banner">
-        <h1>{{ title }}</h1>
-        <p v-if="subtitle">{{ subtitle }}</p>
+        <div class="text">
+            <h1>{{ title }}</h1>
+            <p v-if="subtitle">{{ subtitle }}</p>
+        </div>
+        <div class="backgroundImage"></div>
     </div>
 </template>
 
@@ -25,28 +28,46 @@ const bgImage = `url(${props.imagePath})`;
 
 <style scoped>
 .banner {
-    background-image: v-bind(bgImage);
-    background-size: cover;
-    background-position: center;
     color: white;
     text-align: center;
-    width: 100vw;
+    width: 100%;
     height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 
-    h1, p{
-        margin: 0;
+    .text{
+        position: absolute;
+
+        h1, p{
+            margin: 0;
+        }
+
+        h1{
+            font-size: 16em;
+            font-family: "Night in Tokyo";
+            line-height: 90%;
+        }
+
+        p{
+            font-size: 4em;
+        }
     }
 
-    h1{
-        font-size: 16em;
-    }
-
-    p{
-        font-size: 4em;
+    .backgroundImage{
+        background-image: v-bind(bgImage);
+        filter: blur(4px);
+        -webkit-filter: blur(4px);
+        transform: scale(1.03);
+        height: 100vh;
+        width: 100%;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        z-index: -1;
+        margin: -5px;
+        overflow: hidden;
     }
 }
 </style>
